2e993b9f521beff2ef0d14b2dea04374f2265d67
khairullovruslan <khairullov.ruslan@gmail.com>
2022-04-19 23:09:07 +0300

    fix

--- main.py
+++ main.py
@@ -809,9 +809,9 @@ def delete_account():
 
 
 def main():
-
     """ Запуск сервера"""
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
